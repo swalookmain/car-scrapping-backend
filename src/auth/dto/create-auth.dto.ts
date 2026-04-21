0193ba5bc61bf2b32cc1f6210e723fbf1d2754e6
Sarthak <Sarthak@exato.ai>
2026-04-21 21:52:19 +0530

    nothing

--- src/auth/dto/create-auth.dto.ts
+++ src/auth/dto/create-auth.dto.ts
@@ -1,5 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsString } from 'class-validator';
+// DTO (Data Transfer Object) for creating authentication credentials
 
 export class CreateAuthDto {
   @ApiProperty()
